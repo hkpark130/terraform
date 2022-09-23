c74cf8f14d15ced03e06e3ca9ed2e8363f026a68
hkpark130 <hkpark130@naver.com>
2022-09-24 06:48:33 +0900

    tag 추가

--- terraform/common/dev/services/github.tf
+++ terraform/common/dev/services/github.tf
@@ -4,6 +4,10 @@
 resource "aws_codestarconnections_connection" "github" {
   name          = "hkpark130"
   provider_type = "GitHub"
+
+  tags = {
+    Name = "github-connection"
+  }
 }
 
 output "github_connection" {
